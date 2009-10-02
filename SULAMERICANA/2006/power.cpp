520cf645b6f836aedbd5a761765136d2dba3ce9d
Alexandre Marin <mistermarin@gmail.com>
2009-10-02 00:31:17 -0300

    Sulamericana 2006 Power Generation

---
+++ SULAMERICANA/2006/power.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstring>
+
+#define NN 10058
+#define FOR(a,b) for(int a = 0; a < b; ++a)
+
+int c[NN], p[NN], q[NN], s[NN], x[NN], y[NN], C, N;
+bool v[NN];
+
+inline int sqr(int x){ return (x*x); }
+inline int d2(int a, int b){ return sqr(x[b]-x[a])+sqr(y[b]-y[a]);}
+void reverse(int k, int cap);
+int main(void){
+	while(scanf("%d %d",&N,&C) && N){
+		memset(p, -1, N<<2);
+		memset(s, 0, N<<2);
+		memset(v, 0, N);
+		FOR(i, N) {
+			scanf("%d %d %d",&x[i],&y[i],&c[i]);
+			int min = 1<<25;
+			FOR(j, i){
+				int d = d2(i,j);
+				if(d < min) min = d, p[i] = j;
+			}
+			if(i) s[p[i]]++;
+		}
+		int power_plants = 0;
+		int h, t;
+		h = t = 0;
+		FOR(i, N) if(s[i] == 0) q[t++] = i;
+		while(h != t){
+			int k = q[h++];
+			v[k] = 1;
+			if(c[k] >= C) power_plants++;
+			else if(p[k] != -1)	c[p[k]] += c[k];
+			if(p[k] != -1 && --s[p[k]] == 0) q[t++] = p[k];
+		}
+		FOR(i, N) if(!v[i] && c[i] >= C) power_plants++;
+		printf("%d\n",power_plants);
+	}
+	return 0;
+}
+
