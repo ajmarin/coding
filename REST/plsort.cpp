eec96d9278a2d7d9a8d9a7f508331cf5d8d9e0a1
Alexandre Marin <mistermarin@gmail.com>
2009-10-02 19:21:42 -0300

    DINOSTRA + plsort remake

--- REST/plsort.cpp
+++ REST/plsort.cpp
@@ -2,29 +2,32 @@
 #include <cstring>
 #include <algorithm>
 struct sinfo{
-	char a[50], t[100], d[160];
+	char a[50], t[100], d[200];
 	bool operator<(const sinfo b) const{
 		int x = strcmp(a,b.a);
 		return ((x<0) || (!x && strcmp(t,b.t)<0));
 	}
 } s[1000];
 int main(void){
-	int i = 0, j, len, l, n;
-	for(i = 0; i < 1000 && gets(s[i].d); i++); 
+	int i = 0, j, len, n;
+	register char *p, *q;
+	for(i = 0; i < 1000 && fgets(s[i].d, 200, stdin); i++); 
 	for(n = i, i = 0; i < n; i++){
-		for(j = 0; s[i].d[j]; j++)
-			if(s[i].d[j] == '/') l = j;
-		len = j;
-		for(j = l+1, l = 0; s[i].d[j] != '-' || s[i].d[j+1] != ' '; j++)
-			s[i].a[l++] = s[i].d[j];
-		s[i].a[--l] = 0;
-		for(j = j+2, l = 0; j < len - 3; j++)
-			s[i].t[l++] = s[i].d[j];
-		s[i].t[--l] = 0;
+		for(p = s[i].d; *p; p++)
+			if(*p == '/') break;
+		if(*p != '/') p = s[i].d;
+		else p++;
+		for(q = s[i].a; *p; q++){
+			*q = *p++;
+			if(*q == '-' && *p == ' ') break;
+		}
+		*--q = 0;
+		for(++p, q = s[i].t; *p; ) *q++ = *p++;
+		*(q-5) = 0;
 
 	}
 	std::sort(s,s+n);
-	for(i = 0; i < n; i++) printf("%s\n",s[i].d);
+	for(i = 0; i < n; i++) printf("%s",s[i].d);
 	return 0;
 }
 
--- SPOJ/seletiva/DINOSTRA.cpp
+++ SPOJ/seletiva/DINOSTRA.cpp
@@ -8,7 +8,6 @@ const char *no = "nao\n", *si = "sim\n";
 int main(void){
 	int cnum, f_size, i, j, l_size = 1, sp, N;
 	memset(p,1,sizeof(p));
-	p[0] = p[1] = 0;
 	plist[0] = 2;
 	for(i = 4; i < MAX; i+=2) p[i] = 0;
 	for(i = 3; i < LIM; i+=2)
@@ -26,14 +25,13 @@ int main(void){
 				ffreq[f_size] = 0;
 				while(!(N % plist[i]))
 					N /= plist[i], ffreq[f_size]++;
-				if(ffreq[f_size] > 1) sp++;
-				f_size++;
-				if(N == 1) break;
+				if(ffreq[f_size++] > 1) sp++;
+				if(p[N]) break;
 			}
-			if(i == l_size) f_size++;
+			if(p[N] && N != 1) ffreq[f_size++] = 1;
 			switch(f_size){
 				case 1: (ffreq[0] > 7)	? puts(si):puts(no); break;
-				case 2: (sp > 1 || *ffreq+*(ffreq+1) > 5)?
+				case 2: ((sp > 1) || (ffreq[0]+ffreq[1] > 5))?
 						puts(si):puts(no); break;
 				case 3: (sp)			? puts(si):puts(no); break;
 				default: puts(si); break;
