b0aaa8e30e4e4006d5e361e81bc1d2cb6aac5d08
Alexandre Marin <mistermarin@gmail.com>
2011-01-03 23:32:43 -0200

    Euler 63 improved

--- euler/e063.py
+++ euler/e063.py
@@ -1,6 +1,5 @@
 from math import *
 cnt = 0
 for b in range(1, 10):
-	for p in range(1, 1 + int(1 / (1 - log10(b)))):
-		cnt += len(str(b ** p)) == p
+	cnt += int(1 / (1 - log10(b)))
 print('Answer is', cnt)
