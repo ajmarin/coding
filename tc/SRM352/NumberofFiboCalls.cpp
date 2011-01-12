7ccf8bbed2808b8346c339d536eedadb03850fd7
Alexandre Marin <mistermarin@gmail.com>
2011-01-12 16:22:47 -0200

    SRM352 NumberofFiboCalls // SRM363 HandsShaking

---
+++ tc/SRM352/NumberofFiboCalls.cpp
@@ -0,0 +1,173 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int dp[41][2];
+class NumberofFiboCalls {
+public:
+   vector <int> fiboCallsMade( int n ) {
+	   vector < int > v(2);
+	   dp[0][0] = dp[1][1] = 1;
+	   dp[0][1] = dp[1][0] = 0;
+	   for(int i = 2; i <= n; ++i){
+		   dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+		   dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+	   }
+	   v[0] = dp[n][0];
+	   v[1] = dp[n][1];
+	   return v;
+   }
+};
+
+// BEGIN CUT HERE
+namespace moj_harness {
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				cerr << "Illegal input! Test case " << casenum << " does not exist." << endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			cerr << "No test cases run." << endl;
+		} else if (correct < total) {
+			cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << endl;
+		} else {
+			cerr << "All " << total << " tests passed!" << endl;
+		}
+	}
+	
+	template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << "{"; for (typename vector<T>::const_iterator vi=v.begin(); vi!=v.end(); ++vi) { if (vi != v.begin()) os << ","; os << " " << *vi; } os << " }"; return os; }
+
+	int verify_case(int casenum, const vector <int> &expected, const vector <int> &received, clock_t elapsed) { 
+		cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		cerr << verdict;
+		if (!info.empty()) {
+			cerr << " (";
+			for (int i=0; i<(int)info.size(); ++i) {
+				if (i > 0) cerr << ", ";
+				cerr << info[i];
+			}
+			cerr << ")";
+		}
+		cerr << endl;
+		
+		if (verdict == "FAILED") {
+			cerr << "    Expected: " << expected << endl; 
+			cerr << "    Received: " << received << endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum) {
+		switch (casenum) {
+		case 0: {
+			int n                     = 0;
+			int expected__[]          = {1, 0 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = NumberofFiboCalls().fiboCallsMade(n);
+			return verify_case(casenum, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+		case 1: {
+			int n                     = 3;
+			int expected__[]          = {1, 2 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = NumberofFiboCalls().fiboCallsMade(n);
+			return verify_case(casenum, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+		case 2: {
+			int n                     = 6;
+			int expected__[]          = {5, 8 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = NumberofFiboCalls().fiboCallsMade(n);
+			return verify_case(casenum, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+		case 3: {
+			int n                     = 22;
+			int expected__[]          = {10946, 17711 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = NumberofFiboCalls().fiboCallsMade(n);
+			return verify_case(casenum, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 4: {
+			int n                     = ;
+			int expected__[]          = ;
+
+			clock_t start__           = clock();
+			vector <int> received__   = NumberofFiboCalls().fiboCallsMade(n);
+			return verify_case(casenum, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}*/
+/*      case 5: {
+			int n                     = ;
+			int expected__[]          = ;
+
+			clock_t start__           = clock();
+			vector <int> received__   = NumberofFiboCalls().fiboCallsMade(n);
+			return verify_case(casenum, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}*/
+/*      case 6: {
+			int n                     = ;
+			int expected__[]          = ;
+
+			clock_t start__           = clock();
+			vector <int> received__   = NumberofFiboCalls().fiboCallsMade(n);
+			return verify_case(casenum, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+ 
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(atoi(argv[i]));
+	}
+}
+// END CUT HERE
