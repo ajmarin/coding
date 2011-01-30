d54f9ba95e1d9423b97cdaefb67e30c9d885965f
Alexandre Marin <mistermarin@gmail.com>
2011-01-30 21:41:09 -0200

    Codeforces #4 C (Ad-hoc, count string occurrences)

---
+++ codeforces/004C.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+#define FOR(i,e)		for(int i = 0; i < e; ++i)
+#define FORD(i,s,e)		for(int i = s; i > e; --i)
+#define FOREACH(i,s,e)	for(typeof(s) i = s; i != e; ++i)
+#define FORI(i,s,e)		for(int i = s; i < e; ++i)
+#define IN(x)			(x = getc(stdin))
+#define OUT(x)			putc(x, stdout)
+template <class _T> _T MAX(_T a, _T b){ return a > b ? a : b; }
+template <class _T> _T MIN(_T a, _T b){ return a < b ? a : b; }
+map < string, int > m;
+char name[64];
+int main(void){
+	int n; scanf("%d", &n);
+	FOR(i, n){
+		scanf("%s", name);
+		string os = name;
+		int &x = m[os];
+		if(!x) puts("OK");
+		else printf("%s%d\n", name, x);
+		++x;
+	}
+	return 0;
+}
+
