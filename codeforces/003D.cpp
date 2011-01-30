e0a0f53f219acd1c6805f2e0ad9e52d7736fa40f
Alexandre Marin <mistermarin@gmail.com>
2011-01-30 21:03:02 -0200

    Codeforces #3 D (Greedy, minimal-cost bracket balancing) template upgraded

--- codeforces/003D.cpp
+++ codeforces/003D.cpp
@@ -25,8 +25,30 @@ typedef pair<int, int> pii;
 template <class _T> _T MAX(_T a, _T b){ return a > b ? a : b; }
 template <class _T> _T MIN(_T a, _T b){ return a < b ? a : b; }
 
+char bracket[50024];
+priority_queue < pii > pq;
 int main(void){
-
+	scanf("%s", bracket);
+	int a, b, open = 0;
+	ll c = 0;
+	for(int i = 0; bracket[i]; ++i){
+		open = open + (bracket[i] == '(') - (bracket[i] != '(');
+		if(bracket[i] == '?'){
+			scanf("%d %d", &a, &b);
+			bracket[i] = ')';
+			pq.push(make_pair(-(a - b), i));
+			c += b;
+		}
+		if(open < 0){
+			if(pq.empty()) break;
+			pii x = pq.top(); pq.pop();
+			bracket[x.second] = '(';
+			c += -x.first;
+			open += 2;
+		}
+	}
+	if(open != 0) puts("-1");
+	else cout << c << endl << bracket << endl;
 	return 0;
 }
 
