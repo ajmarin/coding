453dd342d2585cae63359fe711f92ace91e5cf45
Alexandre Marin <mistermarin@gmail.com>
2011-02-01 18:36:15 -0200

    Codeforces #6 C (Ad-hoc, Alice Bob Chocolate)

---
+++ codeforces/006C.cpp
@@ -0,0 +1,43 @@
+
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+#define FOR(i,e)		for(int i = 0; i < e; ++i)
+#define FORD(i,s,e)		for(int i = s; i > e; --i)
+#define FOREACH(i,s,e)	for(typeof(s) i = s; i != e; ++i)
+#define FORI(i,s,e)		for(int i = s; i < e; ++i)
+#define IN(x)			(x = getc(stdin))
+#define OUT(x)			putc(x, stdout)
+template <class _T> _T MAX(_T a, _T b){ return a > b ? a : b; }
+template <class _T> _T MIN(_T a, _T b){ return a < b ? a : b; }
+
+const int N = 100048;
+int v[N];
+int main(void){
+	int a, b, n, ta, tb; scanf("%d", &n);
+	FOR(i, n) scanf("%d", &v[i]);
+	a = ta = tb = 0; b = n;
+	while(a < b){
+		if(ta <= tb) ta += v[a++];
+		else tb += v[--b];
+	}
+	printf("%d %d\n", a, n - a);
+
+	return 0;
+}
+
