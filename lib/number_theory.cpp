da33300babaa6917874cb361b3ceb9ecaa2ab3b4
Alexandre Marin <mistermarin@gmail.com>
2011-09-15 22:12:29 -0300

    SPOJ BR problems moved, number_theory library updated

--- lib/number_theory.cpp
+++ lib/number_theory.cpp
@@ -69,6 +69,24 @@ template <typename _T> _T SumOfDivisors(_T a){
 	if(a != 1) r *= (a + 1);
 	return r - n;
 }
+template<class _T> _T Permutations(_T n){ // Returns n!
+	return n ? Permutations(n - 1) * n : 1;
+}
+template<class _T> _T Combinations(_T n, _T k){ // Returns n! / ( k! (n - k)! )
+	_T r = 1;
+	if(k < n - k) k = n - k;
+	for(_T i = n; i > k; --i) r *= i, r /= (n - i + 1);
+	return r;
+}
+template <class _T> _T PermutationsWithoutFixedPoints(_T n){
+	_T r = 0;
+	for(_T i = 2; i <= n; ++i){
+		_T ways = Combinations(n, i) * Permutations(n - i);
+		if(i & 1) r -= ways;
+		else r += ways;
+	}
+	return r;
+}
 
 int main(void){
 	assert(GCD(30, 42) == 6);
