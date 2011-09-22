f487c4233c0fc9afe68325b38c7d033f1c3cbf70
Alexandre Marin <mistermarin@gmail.com>
2011-09-22 14:31:44 -0300

    UVa 11559

---
+++ uva/Volume CXV/11559.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int main(void){
+	for(int n, b, h, w, z; scanf("%d%d%d%d", &n, &b, &h, &w) == 4; ){
+		int ans = 1 << 30;
+		for(int i = 0; i < h; ++i){
+			int beds = -1, p;
+			scanf("%d", &p);
+			for(int j = 0; j < w; ++j){
+				scanf("%d", &z);
+				if(z > beds) beds = z;
+			}
+			if(beds >= n && p < ans && p * n <= b) ans = p;
+		}
+		if(ans == (1 << 30)) puts("stay home");
+		else printf("%d\n", ans * n);
+	}
+	return 0;
+}
+
