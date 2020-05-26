2489a561bc0bc0a850718ad740ca2ab0569b596f
Alexandre Marin <mistermarin@gmail.com>
2020-05-25 23:36:42 -0700

    memory efficient 1035

--- leetcode/01035_uncrossed_lines.py
+++ leetcode/01035_uncrossed_lines.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
         lena, lenb = len(A), len(B)
-        dp = [[0] * (lenb + 1) for _ in range(lena + 1)]
+        dp = [0] * (lenb + 1)
         for i, a in enumerate(A):
+            prev = dp[0]
             for j, b in enumerate(B):
-                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j], dp[i][j] + (a == b))
-        return dp[lena][lenb]
+                tmp = dp[j + 1]
+                dp[j + 1] = max(dp[j + 1], dp[j], prev + (a == b))
+                prev = tmp
+        return dp[lenb]
