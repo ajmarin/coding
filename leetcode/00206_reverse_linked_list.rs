509c17544a77b2903723ea1c7cceebe0dd79a4c4
Alexandre Marin <mistermarin@gmail.com>
2024-04-04 07:47:08 -0300

    leetcode: 206. Reverse Linked List [rust]

---
+++ leetcode/00206_reverse_linked_list.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let (mut prev, mut head) = (None, head);
+        while let Some(mut node) = head {
+            head = node.next;
+            node.next = prev;
+            prev = Some(node)
+        }
+        prev
+    }
+}
+
