3c36a7fe53b59e37b1488e66939806527895d61a
Alexandre Marin <mistermarin@gmail.com>
2025-07-28 17:16:59 -0300

    leetcode 2913. Subarrays Distinct Element Sum of Squares I

---
+++ leetcode/02913_subarrays_distinct_element_sum_of_squares_i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumCounts(self, nums: List[int]) -> int:
+        ans, n = 0, len(nums)
+        for i in range(n):
+            s = set()
+            for j in range(i, n):
+                s.add(nums[j])
+                ans += len(s) ** 2
+        return ans
+
