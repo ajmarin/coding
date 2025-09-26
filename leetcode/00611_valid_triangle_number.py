eb275ac91882238f176c0b0f15f60d5ef66295e0
Alexandre Marin <mistermarin@gmail.com>
2025-09-26 09:07:04 -0300

    leetcode 611. Valid Triangle (daily)

--- leetcode/00611_valid_triangle_number.py
+++ leetcode/00611_valid_triangle_number.py
@@ -3,12 +3,16 @@ class Solution:
         n = len(nums)
         nums.sort()
         answer = 0
-        for i in range(2, n):
-            left, right = 0, i - 1
+        for i, biggest in enumerate(nums[2:]):
+            left, right = 0, i + 1
+            num_left, num_right = nums[left], nums[right]
             while left < right:
-                if nums[left] + nums[right] > nums[i]:
+                if num_left + num_right > biggest:
                     answer += right - left
                     right -= 1
+                    num_right = nums[right]
                 else:
                     left += 1
+                    num_left = nums[left]
         return answer
+
