3aca65808f8e7523150661d82fcb399965b45799
Alexandre Marin <mistermarin@gmail.com>
2025-10-06 09:22:14 -0300

    leetcode 778. Swim in Rising Water

--- leetcode/00778_swim_in_rising_water.py
+++ leetcode/00778_swim_in_rising_water.py
@@ -1,19 +1,23 @@
+MOVES = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
+
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        best = [[10**6] * n for _ in range(n)]
-        pq = []
-        heapq.heappush(pq, (0, 0, 0))
-        best[0][0] = 0
-        while pq:
-            cost, i, j = heapq.heappop(pq)
-            cost = max(cost, grid[i][j])
-            if i == j == n - 1:
-                return cost
-            for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-                ni, nj = i + di, j + dj
-                if ni < 0 or ni >= n or nj < 0 or nj >= n:
+        N = len(grid)
+        q = [(grid[0][0], (0, 0))]
+        cost = [[inf] * N for _ in range(N)]
+        cost[0][0] = grid[0][0]
+        while q:
+            h, (x, y) = heappop(q)
+            for dx, dy in MOVES:
+                nx, ny = x + dx, y + dy
+                if nx < 0 or nx >= N or ny < 0 or ny >= N:
                     continue
-                if cost < best[ni][nj]:
-                    best[ni][nj] = cost
-                    heapq.heappush(pq, (cost, ni, nj))
+                if h >= cost[nx][ny]:
+                    continue
+                nh = cost[nx][ny] = h if h > grid[nx][ny] else grid[nx][ny]
+                if nx == ny == N - 1:
+                    return nh
+                heappush(q, (nh, (nx, ny)))
+        return grid[0][0]
+
