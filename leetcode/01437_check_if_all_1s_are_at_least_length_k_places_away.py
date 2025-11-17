9b8c1dcc1bda547862b9aa2e6f4ccf5ddf2be509
Alexandre Marin <mistermarin@gmail.com>
2025-11-17 09:04:25 -0300

    leetcode 1437. Check If All 1's Are at Least Length K Places Away

--- leetcode/01437_check_if_all_1s_are_at_least_length_k_places_away.py
+++ leetcode/01437_check_if_all_1s_are_at_least_length_k_places_away.py
@@ -1,4 +1,10 @@
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
-        x = tuple(i for i, j in enumerate(nums) if j)
-        return all(x[i] - x[i - 1] > k for i in range(1, len(x)))
+        last = -(10**6)
+        for i, n in enumerate(nums):
+            if n == 1:
+                if i - last <= k:
+                    return False
+                last = i
+        return True
+
