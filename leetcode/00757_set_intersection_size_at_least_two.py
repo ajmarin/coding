464fbe5cad091c7a7a2b0a790a68011ba25085e2
Alexandre Marin <mistermarin@gmail.com>
2025-11-20 09:06:38 -0300

    leetcode 757. Set Intersection Size At Least Two

--- leetcode/00757_set_intersection_size_at_least_two.py
+++ leetcode/00757_set_intersection_size_at_least_two.py
@@ -1,21 +1,14 @@
 class Solution:
     def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        index = 0
-        for (start, end) in intervals:
-            while index and intervals[index - 1][1] >= end:
-                index -= 1
-            if index and start == intervals[index - 1][0]:
-                continue
-            intervals[index] = start, end
-            index += 1
-        ans, second, largest = 0, -2, -1
-        for start, end in intervals[:index]:
-            largest_in = start <= largest
-            second_in = start <= second
-            if largest_in and second_in:
-                continue
-            ans += 2 - largest_in
-            second = largest if largest_in else end - 1
-            largest = end
+        intervals.sort(key=itemgetter(1))
+        ans = 0
+        second, biggest = -1, -1
+        for start, end in intervals:
+            if biggest < start:
+                second, biggest = end - 1, end
+                ans += 2
+            elif second < start:
+                second, biggest = biggest - (biggest == end), end
+                ans += 1
         return ans
+
