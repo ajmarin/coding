b5009786b6ae75978148c1f2761e0801b29991e3
Alexandre Marin <mistermarin@gmail.com>
2025-11-23 07:42:11 -0300

    leetcode 1262. Greatest Sum Divisible by Three

--- leetcode/01262_greatest_sum_divisible_by_three.py
+++ leetcode/01262_greatest_sum_divisible_by_three.py
@@ -1,13 +1,30 @@
+MAX_N = 10**6
+
+
+def get_mins(seq):
+    lowest, second = MAX_N, MAX_N
+    for n in seq:
+        if n < lowest:
+            lowest, second = n, lowest
+        elif n < second:
+            second = n
+    return lowest, second
+
+
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        s = sum(nums)
-        mod3 = collections.defaultdict(list)
-        for n in nums:
-            mod3[n % 3].append(n)
-        rem, sub = s % 3, 0
-        if rem:
-            sub = min(mod3[rem])
-            if len(mod3[3 - rem]) > 1:
-                mod3[3 - rem].sort()
-                sub = min(sub, sum(mod3[3 - rem][:2]))
-        return s - sub
+        total = sum(nums)
+        remainder = total % 3
+        if remainder == 0:
+            return total
+
+        mod1 = get_mins([n for n in nums if n % 3 == 1])
+        mod2 = get_mins([n for n in nums if n % 3 == 2])
+
+        remove = MAX_N
+        if remainder == 1:
+            remove = min(mod1[0], sum(mod2))
+        else:
+            remove = min(mod2[0], sum(mod1))
+        return total - remove
+
