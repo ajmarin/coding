bf0b38b854a976c656077fca853c14c9d50bc8a4
Alexandre Marin <mistermarin@gmail.com>
2026-03-26 13:44:21 -0300

    leetcode 338. Counting Bits

---
+++ leetcode/00338_counting_bits.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0] * (n + 1)
+        for i in range(1, n + 1):
+            res[i] = res[i >> 1] + (i & 1)
+        return res
