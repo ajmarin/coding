b387be5e19fff9a2d53f304b2e59ced443e51cca
Alexandre Marin <mistermarin@gmail.com>
2026-04-07 15:36:56 -0300

    leetcode 2351. First Letter to Appear Twice

---
+++ leetcode/02351_first_letter_to_appear_twice.py
@@ -0,0 +1,7 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        seen = set()
+        for c in s:
+            if c in seen:
+                return c
+            seen.add(c)
