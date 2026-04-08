1970ffb8c3aad5f6799e6f9816fc50c8dd16d941
Alexandre Marin <mistermarin@gmail.com>
2026-04-08 15:13:32 -0300

    leetcode 2634. Filter Elements from Array

---
+++ leetcode/02634_filter_elements_from_array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  const res = [];
+  for (let i = 0; i < arr.length; ++i) if (fn(arr[i], i)) res.push(arr[i]);
+  return res;
+};
