21330a5a8159d0ce11600135f8ad1454816c6a26
Alexandre Marin <mistermarin@gmail.com>
2026-04-08 15:14:41 -0300

    leetcode 2635. Apply Transform Over Each Element in Array

---
+++ leetcode/02635_apply_transform_over_each_element_in_array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+  const res = [];
+  for (let i = 0; i < arr.length; ++i) res.push(fn(arr[i], i));
+  return res;
+};
