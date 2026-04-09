022d3b0c67134f5261368f04386d8dfb6844189e
Alexandre Marin <mistermarin@gmail.com>
2026-04-09 16:03:36 -0300

    leetcode 2715. Timeout Cancellation

---
+++ leetcode/02715_timeout_cancellation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  const timeout = setTimeout(() => fn(...args), t);
+  return () => clearTimeout(timeout);
+};
