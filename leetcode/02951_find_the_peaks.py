86a101fb040da74990daf5dc74e9d2ab4f50f49d
Alexandre Marin <mistermarin@gmail.com>
2026-04-10 16:31:00 -0300

    leetcode 2951. Find the Peaks

---
+++ leetcode/02951_find_the_peaks.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        n = len(mountain)
+        return [
+            i
+            for i in range(1, n - 1)
+            if mountain[i - 1] < mountain[i] > mountain[i + 1]
+        ]
